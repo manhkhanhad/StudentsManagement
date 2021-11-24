72598d043215bee3f54448c447cdd0a30871680f
Ngo Khanh <manhkhanhad@gmail.com>
2021-11-24 15:55:46 +0700

    update database

---
+++ database/qlhs_v2.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 24, 2021 at 08:52 AM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bangdiem`
+--
+
+DROP TABLE IF EXISTS `bangdiem`;
+CREATE TABLE IF NOT EXISTS `bangdiem` (
+  `MaBangDiem` varchar(15) NOT NULL,
+  `MaMon` varchar(3) NOT NULL,
+  `MaLop` varchar(4) NOT NULL,
+  `MaHK` varchar(3) NOT NULL,
+  `SLDMon` decimal(5,2) NOT NULL DEFAULT '0.00',
+  `TLDMon` decimal(5,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`MaBangDiem`),
+  KEY `MaMon` (`MaMon`),
+  KEY `MaLop` (`MaLop`),
+  KEY `MaHK` (`MaHK`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chitietbangdiem`
+--
+
+DROP TABLE IF EXISTS `chitietbangdiem`;
+CREATE TABLE IF NOT EXISTS `chitietbangdiem` (
+  `MaChiTietBangDiem` varchar(23) NOT NULL,
+  `MaBangDiem` varchar(15) NOT NULL,
+  `MaHS` varchar(8) NOT NULL,
+  `Diem15p` decimal(3,1) DEFAULT NULL,
+  `Diem1Tiet` decimal(3,1) DEFAULT NULL,
+  `DiemHK` decimal(3,1) DEFAULT NULL,
+  `DiemTB` decimal(3,1) GENERATED ALWAYS AS ((((`Diem15p` + (`Diem1Tiet` * 2)) + (`DiemHK` * 3)) / (((`Diem15p` is not null) + ((`Diem1Tiet` is not null) * 2)) + ((`DiemHK` is not null) * 3)))) STORED,
+  KEY `MaBangDiem` (`MaBangDiem`),
+  KEY `MaHS` (`MaHS`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hocki`
+--
+
+DROP TABLE IF EXISTS `hocki`;
+CREATE TABLE IF NOT EXISTS `hocki` (
+  `MaHK` varchar(3) NOT NULL,
+  `TenHK` varchar(3) NOT NULL,
+  PRIMARY KEY (`MaHK`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hocki`
+--
+
+INSERT INTO `hocki` (`MaHK`, `TenHK`) VALUES
+('HK1', 'HK1'),
+('HK2', 'HK2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hocsinh`
+--
+
+DROP TABLE IF EXISTS `hocsinh`;
+CREATE TABLE IF NOT EXISTS `hocsinh` (
+  `MaHS` varchar(8) NOT NULL,
+  `MaTK` varchar(10) NOT NULL,
+  `HoTen` varchar(50) NOT NULL,
+  `GioiTinh` varchar(3) NOT NULL,
+  `NgaySinh` date NOT NULL,
+  `DiaChi` varchar(50) NOT NULL,
+  `Email` varchar(20) NOT NULL,
+  `MaLop` varchar(8) NOT NULL,
+  PRIMARY KEY (`MaHS`),
+  KEY `MaLop` (`MaLop`),
+  KEY `MaTK` (`MaTK`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hocsinh`
+--
+
+INSERT INTO `hocsinh` (`MaHS`, `MaTK`, `HoTen`, `GioiTinh`, `NgaySinh`, `DiaChi`, `Email`, `MaLop`) VALUES
+('19220001', 'HS19220001', 'Nguyen Van A', 'Nam', '2004-01-27', 'TpHCM', 'nguyenvana@gmail.com', '12A1'),
+('19220002', 'HS19220002', 'Tran Ngoc Han', 'Nu', '2004-03-11', 'Kien Giang', 'ngochan@gmail.com', '12A1'),
+('19220003', 'HS19220003', 'Tran Ngoc Linh', 'Nu', '2004-05-24', 'Tay Ninh', 'ngoclinh@gmail.com', '12A2'),
+('19220004', 'HS19220004', 'Le Nhat Minh', 'Nam', '2004-07-06', 'Nghe An', 'minhtam@gmail.com', '12A2'),
+('19220005', 'HS19220005', 'Phan Thi Thanh Tam', 'Nu', '2004-01-09', 'Vinh Long', 'thanhtam@gmail.com', '12A13'),
+('20230001', 'HS20230001', 'Phan Thanh Trieu', 'Nam', '2005-02-12', 'Dong Nai', 'thanhtrieu@gmail.com', '11A1'),
+('20230002', 'HS20230002', 'Le Quang Hien', 'Nam', '2005-06-20', 'TpHCM', 'quanghien@gmail.com', '11A3'),
+('20230003', 'HS20230003', 'Tran Thi Hong Tham', 'Nu', '2005-01-10', 'TpHCM', 'hongtham@gmail.com', '11A2'),
+('20230004', 'HS20230004', 'Do Thi Xuan', 'Nu', '2005-04-03', 'Binh Duong', 'doxuan@gmail.com', '11A3'),
+('20230005', 'HS20230005', 'Nguyen Thi Kim Cuc', 'Nu', '2005-06-07', 'TpHCM', 'kimcuc@gmail.com', '11A2'),
+('21240001', 'HS21240001', 'Tran Trung Hieu', 'Nam', '2006-04-23', 'TpHCM', 'trunghieu@gmail.com', '10A1'),
+('21240002', 'HS21240002', 'Truong My Hanh', 'Nu', '2006-06-15', 'Tay Ninh', 'myhanh@gmail.com', '10A1'),
+('21240003', 'HS21240003', 'Ha Duy Lap', 'Nam', '2006-03-19', 'Ben Tre', 'duylap@gmail.com', '10A2'),
+('21240004', 'HS21240004', 'Ngo Thanh Tuan', 'Nam', '2006-07-17', 'Dong Nai', 'thanhtuan@gmail.com', '10A2'),
+('21240005', 'HS21240005', 'Tran Minh Man', 'Nam', '2006-10-15', 'Binh Duong', 'minhman@gmail.com', '10A3'),
+('21240006', 'HS21240006', 'Nguyen Trung Nghia', 'Nam', '2006-12-12', 'Binh Phuoc', 'trungnghia@gmail.com', '10A3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lop`
+--
+
+DROP TABLE IF EXISTS `lop`;
+CREATE TABLE IF NOT EXISTS `lop` (
+  `MaLop` varchar(4) NOT NULL,
+  `TenLop` varchar(4) NOT NULL,
+  `Khoi` varchar(2) NOT NULL,
+  `SiSo` int(11) NOT NULL,
+  `SLDLop` decimal(3,2) NOT NULL DEFAULT '0',
+  `TLDLop` decimal(3,2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`MaLop`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lop`
+--
+
+INSERT INTO `lop` (`MaLop`, `TenLop`, `Khoi`, `SiSo`, `SLDLop`, `TLDLop`) VALUES
+('10A1', '10A1', '10', 30, '0', '0'),
+('10A2', '10A2', '10', 30, '0', '0'),
+('10A3', '10A3', '10', 30, '0', '0'),
+('11A1', '11A1', '11', 30, '0', '0'),
+('11A2', '11A2', '11', 30, '0', '0'),
+('11A3', '11A3', '11', 30, '0', '0'),
+('12A1', '12A1', '12', 30, '0', '0'),
+('12A2', '12A2', '12', 30, '0', '0'),
+('12A3', '12A3', '12', 30, '0', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `monhoc`
+--
+
+DROP TABLE IF EXISTS `monhoc`;
+CREATE TABLE IF NOT EXISTS `monhoc` (
+  `MaMH` varchar(3) NOT NULL,
+  `TENMH` varchar(20) NOT NULL,
+  PRIMARY KEY (`MaMH`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `monhoc`
+--
+
+INSERT INTO `monhoc` (`MaMH`, `TENMH`) VALUES
+('TOA', 'TOAN'),
+('VLY', 'Vat ly'),
+('HOA', 'Hoa hoc'),
+('SIN', 'Sinh hoc'),
+('LSU', 'Lich su'),
+('DIA', 'Dia ly'),
+('VAN', 'Ngu van'),
+('DDU', 'Dao duc'),
+('TDU', 'The duc');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taikhoan`
+--
+
+DROP TABLE IF EXISTS `taikhoan`;
+CREATE TABLE IF NOT EXISTS `taikhoan` (
+  `MaTK` varchar(10) NOT NULL,
+  `TenDN` varchar(50) NOT NULL,
+  `MatKhau` varchar(255) NOT NULL,
+  `ChucVu` varchar(11) NOT NULL,
+  PRIMARY KEY (`MaTK`),
+  UNIQUE KEY `TenDN` (`TenDN`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `taikhoan`
+--
+
+INSERT INTO `taikhoan` (`MaTK`, `TenDN`, `MatKhau`, `ChucVu`) VALUES
+('HS19220001', '19220001', '123456', 'HocSinh'),
+('HS19220002', '19220002', '123456', 'HocSinh'),
+('HS19220003', '19220003', '123456', 'HocSinh'),
+('HS19220004', '19220004', '123456', 'HocSinh'),
+('HS19220005', '19220005', '123456', 'HocSinh'),
+('HS20230001', '20230001', '123456', 'HocSinh'),
+('HS20230002', '20230002', '123456', 'HocSinh'),
+('HS20230003', '20230003', '123456', 'HocSinh'),
+('HS20230004', '20230004', '123456', 'HocSinh'),
+('HS20230005', '20230005', '123456', 'HocSinh'),
+('HS21240001', '21240001', '123456', 'HocSinh'),
+('HS21240002', '21240002', '123456', 'HocSinh'),
+('HS21240003', '21240003', '123456', 'HocSinh'),
+('HS21240004', '21240004', '123456', 'HocSinh'),
+('HS21240005', '21240005', '123456', 'HocSinh');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
